725cf22faeac80a6675fd8bba0602c2aec14926e
Kevin Alles <git-message@kevin-alles.de>
2025-09-19 14:09:54 +0200

    initialize project structure with Maven and Spring Boot setup

---
+++ src/test/java/de/kevinalles/quizbackend/QuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kevinalles.quizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
