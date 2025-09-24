b3b7319eb55453c92835c64f484602cedbd297dc
Kevin Alles <git-message@kevin-alles.de>
2025-09-24 14:14:00 +0200

    add logging to ReachableController for backend reachability check

--- src/main/java/de/kevinalles/quizbackend/controller/ReachableController.java
+++ src/main/java/de/kevinalles/quizbackend/controller/ReachableController.java
@@ -1,14 +1,17 @@
 package de.kevinalles.quizbackend.controller;
 
+import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log
 @RestController
 public class ReachableController {
 
     // Endpoint to check if the backend is reachable
     @PostMapping("/reachable")
     public String isReachable() {
+        log.info("Backend is reachable");
         return "Backend is reachable";
     }
 }
